72e71618fdee0240498330ee2e487816c57e1ebc
dietcokechan <dietcokechan@gmail.com>
2023-06-15 07:42:41 -0700

    Print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Return pint
+ * Return: Always 0 (Success)
+ * Prints a string in the terminal
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
