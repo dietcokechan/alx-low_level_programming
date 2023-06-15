3169b3669fc93748fe7a83dae70fdc9c94b0f6b1
dietcokechan <dietcokechan@gmail.com>
2023-06-15 07:46:34 -0700

    new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - Return pint
  * Return: Always 0 (Success)
+ *
  * Prints a string in the terminal
- */
+ **/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
