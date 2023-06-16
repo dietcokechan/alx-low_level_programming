1904374733e5f6a7d3f63578d62430efbd5ed412
dietcokechan <dietcokechan@gmail.com>
2023-06-16 03:20:14 -0700

    Prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
