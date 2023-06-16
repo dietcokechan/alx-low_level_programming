a42580eaefc32f6eb0563a2fbd3add51cb292a45
dietcokechan <dietcokechan@gmail.com>
2023-06-16 03:54:52 -0700

    Prints formatted numbers in sequence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num != 9)
+		{
+			putchar(48 + num);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar(48 + num);
+
+		num++;
+	}
+	return (0);
+}
