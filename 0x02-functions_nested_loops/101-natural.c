9d030c856978938e13229f428d64cd68e3dd8813
dietcokechan <dietcokechan@gmail.com>
2023-06-20 15:24:25 -0700

    List all natural nums below 10 that are multiple of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+	int y = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			y += x;
+		}
+	}
+	printf("%d\n", y);
+	return (0);
+}
