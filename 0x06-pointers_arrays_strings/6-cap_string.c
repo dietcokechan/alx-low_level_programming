ad387577a96f2ee60c5fe7e54ba9444b9c9ecb52
dietcokechan <dietcokechan@gmail.com>
2023-06-29 15:29:46 -0700

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * check_separators - checks if separator is present
+ *
+ * @s: character to check
+ *
+ * Return: true or false
+ */
+int check_separators(char s)
+{
+	int i;
+	char sepArr[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (sepArr[i] == s)
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * cap_string - capitalizes all words of a string
  *
  * @str: string
@@ -13,22 +33,12 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
+		if (check_separators(str[i]))
 			continue;
-		}
 
-		if (str[i] == ' ')
+		if (str[i] >= 'a' && str[i] <= 'z' && (check_separators(str[i - 1]) || i == 0))
 		{
-			++i;
-
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
+			str[i] = str[i] - 32;
 		}
 		else
 		{
