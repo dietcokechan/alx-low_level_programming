e9409dcc91c82131a3ea0ecdf5d1c52618a6b8c7
dietcokechan <dietcokechan@gmail.com>
2023-07-05 14:14:27 -0700

    returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * check_num - checks if num has square root
+ *
+ * @x: first num
+ * @y: second num
+ *
+ * Return: num
+ */
+int check_num(int x, int y)
+{
+	if (x * x == y)
+		return(x);
+	if (x * x > y)
+		return (-1);
+	return (check_num(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: input
+ *
+ * Return: -1 if n doesn't have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (check_num(i, n) == n && n != 1)
+		return (-1);
+	return (check_num(i, n));
+}
