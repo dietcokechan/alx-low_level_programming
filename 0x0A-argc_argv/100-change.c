2d8927ddf4e382603b27e6394f44cdf65f72f110
dietcokechan <dietcokechan@gmail.com>
2023-07-07 15:21:14 -0700

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,25 +28,15 @@ int main(int argc, char *argv[])
 		while (cents > 0)
 		{
 			if (cents % 25 == 0)
-			{
 				cents -= 25;
-			}
 			else if (cents % 10 == 0)
-			{
 				cents -= 10;
-			}
 			else if (cents % 5 == 0)
-			{
 				cents -= 5;
-			}
 			else if (cents % 2 == 0)
-			{
 				cents -= 2;
-			}
 			else
-			{
 				cents--;
-			}
 			coins++;
 		}
 		printf("%d\n", coins);
