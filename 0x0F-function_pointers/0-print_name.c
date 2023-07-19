dc69152ffcfc1205ba311bb82d4024c5e2451125
dietcokechan <dietcokechan@gmail.com>
2023-07-19 05:12:57 -0700

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name
+ * @f: pointer to function
+ *
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
