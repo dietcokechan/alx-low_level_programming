319b7df60bad46b60d68bbb44b5f77fbd01306e6
dietcokechan <dietcokechan@gmail.com>
2023-07-19 06:34:24 -0700

    get opcodes of main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_opcodes - print the opcode of program
+ *
+ * @a: address
+ * @n: number of bytes
+ *
+ * Return:  void
+ */
+void get_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < (n - 1))
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ *
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	get_opcodes((char *)&main, n);
+	return (0);
+}
