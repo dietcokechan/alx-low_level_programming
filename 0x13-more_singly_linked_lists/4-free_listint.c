a4cf0b1616daf1520de2108bdfabd5734c9a827b
dietcokechan <dietcokechan@gmail.com>
2023-08-01 05:48:43 -0700

    frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ *
+ * @head: head
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		free(head);
+		head = temp->next;
+	}
+}
