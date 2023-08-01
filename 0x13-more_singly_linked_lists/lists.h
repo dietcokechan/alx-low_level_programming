21c843de64a97a0958d7bee9e32a2a0865a3d530
dietcokechan <dietcokechan@gmail.com>
2023-08-01 05:23:37 -0700

    prints all the elements of a list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,38 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
