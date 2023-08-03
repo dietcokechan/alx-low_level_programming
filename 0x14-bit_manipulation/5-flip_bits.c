56cd6674aec27a6d1dcedc6e70289d5045034362
dietcokechan <dietcokechan@gmail.com>
2023-08-03 07:59:28 -0700

    returns num of flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number fo bits you need to flip
+ *
+ * @n: num
+ * @m: target
+ *
+ * Return: num of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flips, count = 0;
+
+	flips = n ^ m;
+	while (flips > 0)
+	{
+		count += flips & 1;
+		flips >>= 1;
+	}
+	return (count);
+}
