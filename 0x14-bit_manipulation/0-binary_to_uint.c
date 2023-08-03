9549f6cdcf5f4fa2510391ecef2189a1c6c464e2
dietcokechan <dietcokechan@gmail.com>
2023-08-03 07:28:44 -0700

    converts binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  *
  * @b: pointer to binary num
  *
