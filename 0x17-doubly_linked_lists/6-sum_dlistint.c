4e70f84c2814b3f8ab37243f33ef02632ccd3c71
dietcokechan <dietcokechan@gmail.com>
2023-10-13 01:20:18 +0300

    betty fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,14 +7,14 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    const dlistint_t *temp;
-    int sum = 0;
+	const dlistint_t *temp;
+	int sum = 0;
 
-    temp = head;
-    while (temp != NULL)
-    {
-        sum += temp->n;
-        temp = temp->next;
-    }
-    return (sum);
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
 }
