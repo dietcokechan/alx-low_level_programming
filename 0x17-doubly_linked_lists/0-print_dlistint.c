c795506f2d426a45352fb15b764a27a1ab0e3244
dietcokechan <dietcokechan@gmail.com>
2023-10-13 00:41:03 +0300

    betty fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *temp;
-    int i = 0;
+	const dlistint_t *temp;
+	int i = 0;
 
-    temp = h;
-    while (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-        i++;
-    }
-    return (i);
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
 }
