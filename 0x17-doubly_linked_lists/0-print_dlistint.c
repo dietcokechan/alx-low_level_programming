a85e79070b131fdbd7236abe43c390dcf806b7db
dietcokechan <dietcokechan@gmail.com>
2023-10-13 00:38:59 +0300

    prints all elements in list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements in list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *temp;
+    int i = 0;
+
+    temp = h;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+        i++;
+    }
+    return (i);
+}
