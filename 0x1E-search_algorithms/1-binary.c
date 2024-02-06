09d57d6eb0974c16fd69c576e343d0af445ba1ec
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-06 20:26:38 +0200

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for value in sorted array of ints
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: index of value or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, l, r;
+
+	if (array == NULL)
+		return (-1);
+
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = l + (r - l) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+
+	return (-1);
+}
